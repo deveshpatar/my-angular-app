3746516d76f613e5451782d82b79a747b4af6fc3
Devesh Kumar Patar <deveshpatar@gmail.com>
2020-02-26 11:19:42 +0530

    CRUD Operations in Rest API using angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,33 @@ export class AppComponent implements OnInit{
   }
 
   listOfCustomer: Customer[];
+  customerDetails: Customer;
+  custObject: Customer;
 
   ngOnInit(){
     this.customerService.getCustomer().subscribe(
-      data => 
+      data =>  
       {
           this.listOfCustomer = data;
     });
+
+    this.customerService.getCustomerDetails().subscribe(
+      data =>{
+          this.customerDetails = data;
+      }
+    );
+
+    var custObj = new Customer();
+    custObj.customerName = 'Advik';
+    custObj.customerSex = 'Male';
+    custObj.customerAdd1 = 'Ambikapur';
+    custObj.cusromerAdd2 = 'Surguja';
+
+    this.customerService.createCustomer(custObj).subscribe(
+      data =>{
+        this.custObject = data;
+      }
+    );
   }
 
 }
